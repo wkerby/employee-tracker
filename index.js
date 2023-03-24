bd206748c7cec16816d4bb980cd2a53d5e2d1745
William Kerby <w.kerby99@gmail.com>
2023-03-23 23:50:18 -0400

    create role add function

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const doNext = () => {
             case "View all employees":
                 pass;
             case "Add a department":
-                pass;
+                departmentAdd();
             case "Add a role":
                 pass;
             case "Add an employee":
@@ -41,7 +41,7 @@ const doNext = () => {
     })
 }
 
-//add function to ask department add questions
+//add function to ask department add questions and to add department to employee_db
 const departmentAdd = () => {
     let departmentAddQuestion = [
         {
@@ -55,7 +55,48 @@ const departmentAdd = () => {
     inquirer.prompt(departmentAddQuestion).then(answers => {
         let employeeData = new Query();
         employeeData.addDepartment(answers.departmentname);
+        try {
+            employeeData.addDepartment(answers.departmentname);
+        }
+
+        catch (err) {
+            console.log("Please enter a valid department name.");
+            departmentAdd();
+        }
+
 
     })
 
+}
+
+//add function to ask role add questions and to add role to employee_db
+const roleAdd = () => {
+    let roleAddQuestions = [
+        {
+            type: 'input',
+            message: 'What is the name of the role?',
+            name: 'rolename',
+        },
+        {
+            type: 'input',
+            message: 'What is the salary of the role?',
+            name: 'rolesalary',
+        },
+        {
+            type: 'input',
+            message: 'To which department does the role belong?',
+            name: 'roledepartment',
+        }
+    ]
+    inquire.prompt(roleAddQuestions).then(answers => {
+        let employeeData = new Query();
+        try {
+            employeeData.addRole(answers.rolename, answers.rolesalary, answers.roledepartment);
+        }
+        catch (err) {
+            console.log("Department name is not valid. Please try again.")
+            roleAdd();
+        }
+
+    })
 }
