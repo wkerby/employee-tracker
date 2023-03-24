cd5634441934067c2c1fbc8d4eea6e4829ce6eb1
William Kerby <w.kerby99@gmail.com>
2023-03-24 10:27:55 -0400

    make changes to functions to incorporate object of Query class

--- index.js
+++ index.js
@@ -20,32 +20,40 @@ const doNext = () => {
 
     ]
     inquirer.prompt(doNextQuestion).then(answers => {
-        switch (answers.next) {
-            case "View all departments":
-                getDepartments();
-            case "View all roles":
-                getRoles();
-            case "View all employees":
-                getEmployees();
-            case "Add a department":
-                departmentAdd();
-            case "Add a role":
-                roleAdd();
-            case "Add an employee":
-                employeeAdd();
-            case "Update an employee role":
-                pass;
-            case "Quit":
-                pass;
+        if (answers.next != "Quit") {
+            let employeeData = new Query();
+            switch (answers.next) {
+                case "View all departments":
+                    getDepartments(employeeData);
+                case "View all roles":
+                    getRoles(employeeData);
+                case "View all employees":
+                    getEmployees(employeeData);
+                case "Add a department":
+                    departmentAdd(employeeData);
+                case "Add a role":
+                    roleAdd(employeeData);
+                case "Add an employee":
+                    employeeAdd(employeeData);
+                case "Update an employee role":
+                    pass;
+            }
+
+            doNext();
+
         }
+
+        else {
+            pass;
+        }
+
     })
 }
 
 //add function to return all departments to user
-const getDepartments = () => {
-    let employeeData = new Query();
+const getDepartments = (query) => {
     try {
-        employeeData.viewDepartments();
+        query.viewDepartments();
     }
     catch (err) {
         console.log("There was an issue retrieving department data. Please try again.");
@@ -54,10 +62,9 @@ const getDepartments = () => {
 }
 
 //add function to return all roles to user
-const getRoles = () => {
-    let employeeData = new Query();
+const getRoles = (query) => {
     try {
-        employeeData.viewRoles();
+        query.viewRoles();
     }
     catch (err) {
         console.log("There was an issue retrieving role data. Please try again.")
@@ -66,10 +73,9 @@ const getRoles = () => {
 }
 
 //add function to return all employees to user
-const getEmployees = () => {
-    let employeeData = new Query();
+const getEmployees = (query) => {
     try {
-        employeeData.viewEmployees();
+        query.viewEmployees();
     }
     catch (err) {
         console.log("There was an issue retrieving employee data. Please try again.")
@@ -78,7 +84,7 @@ const getEmployees = () => {
 }
 
 //add function to ask department add questions and to add department to employee_db
-const departmentAdd = () => {
+const departmentAdd = (query) => {
     let departmentAddQuestion = [
         {
             type: 'input',
@@ -89,10 +95,9 @@ const departmentAdd = () => {
     ]
 
     inquirer.prompt(departmentAddQuestion).then(answers => {
-        let employeeData = new Query();
-        employeeData.addDepartment(answers.departmentname);
+
         try {
-            employeeData.addDepartment(answers.departmentname);
+            query.addDepartment(answers.departmentname);
         }
 
         catch (err) {
@@ -106,7 +111,7 @@ const departmentAdd = () => {
 }
 
 //add function to ask role add questions and to add role to employee_db
-const roleAdd = () => {
+const roleAdd = (query) => {
     let roleAddQuestions = [
         {
             type: 'input',
@@ -125,9 +130,8 @@ const roleAdd = () => {
         }
     ]
     inquirer.prompt(roleAddQuestions).then(answers => {
-        let employeeData = new Query();
         try {
-            employeeData.addRole(answers.rolename, answers.rolesalary, answers.roledepartment);
+            query.addRole(answers.rolename, answers.rolesalary, answers.roledepartment);
         }
         catch (err) {
             console.log("Department name is not valid. Please try again.")
@@ -138,7 +142,7 @@ const roleAdd = () => {
 }
 
 //add function to ask employee add questions
-const employeeAdd = () => {
+const employeeAdd = (query) => {
     let employeeAddQuestions = [
         {
             type: 'input',
@@ -162,9 +166,8 @@ const employeeAdd = () => {
         }
     ]
     inquirer.prompt(employeeAddQuestions).then(answers => {
-        let employeeData = new Query();
         try {
-            employeeData.addEmployee(answers.employeefirst, answers.employeelast, answers.employeerole, answers.employeemanager);
+            query.addEmployee(answers.employeefirst, answers.employeelast, answers.employeerole, answers.employeemanager);
         }
         catch (err) {
             console.log("There was an error adding this new employee. Please try again");
